1e2bde1e2ba1a8f361d721c1069074b3f18010c2
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2021-07-08 19:40:16 +0800

    v2

--- send_tokens.py
+++ send_tokens.py
@@ -18,18 +18,18 @@ headers = {
 acl = algod.AlgodClient(algod_token, algod_address, headers)
 min_balance = 100000 #https://developer.algorand.org/docs/features/accounts/#minimum-balance
 
-private_key, public_address = account.generate_account()
-print("Base64 Private Key: {}\nPublic Algorand Address: {}\n".format(private_key, public_address))
-mnemonic_secret = mnemonic.from_private_key(private_key)
+#private_key, public_address = account.generate_account()
+#print("Base64 Private Key: {}\nPublic Algorand Address: {}\n".format(private_key, public_address))
+#mnemonic_secret = mnemonic.from_private_key(private_key)
 #print("mnemonic_secret = \"{}\"".format(mnemonic_secret))
 #mnemonic_secret = "daughter coin decorate junior shift much soft pottery change chase they family culture program salute stool angry chapter cake congress sugar castle inject above stable"
 mnemonic1 = "daughter coin decorate junior shift much soft pottery change chase they family culture program salute stool angry chapter cake congress sugar castle inject above stable"
 sk = mnemonic.to_private_key(mnemonic1)
 pk = mnemonic.to_public_key(mnemonic1)
-print("My address: {}".format(pk))
+#print("My address: {}".format(pk))
 account_info = acl.account_info(pk)
-print(json.dumps(account_info, indent=4))
-print("Account balance: {} microAlgos".format(account_info.get('amount')))
+#print(json.dumps(account_info, indent=4))
+#print("Account balance: {} microAlgos".format(account_info.get('amount')))
 
 
 def send_tokens( receiver_pk, tx_amount ):
